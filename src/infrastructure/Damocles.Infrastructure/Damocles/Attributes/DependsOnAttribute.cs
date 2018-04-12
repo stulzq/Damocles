602101f645177e69a61bb64ce30fe4764eb2ecdb
stulzq <stulzq@qq.com>
2018-04-13 00:38:04 +0800

    添加 基础设置 模块功能

---
+++ src/infrastructure/Damocles.Infrastructure/Damocles/Attributes/DependsOnAttribute.cs
@@ -0,0 +1,42 @@
+﻿// #region File Annotation
+// 
+// Author：Zhiqiang Li
+// 
+// FileName：DependsOnAttribute.cs
+// 
+// Project：Damocles
+// 
+// CreateDate：2018年04月13日
+// 
+// ProjectUrl：https://github.com/stulzq/Damocles
+// 
+// .NET Core China Open Source organization  PMC member. https://github.com/dotnetcore
+// 
+// Note: The reference to this document code must not delete this note, and indicate the source!
+// 
+// #endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Damocles.Attributes
+{
+	/// <summary>
+	/// 模块依赖特性 
+	/// <para />
+	/// 指定改模块依赖的模块
+	/// </summary>
+	public class DependsOnAttribute:Attribute
+	{
+		/// <summary>
+		/// 该模块依赖的模块集合
+		/// </summary>
+		public List<Type> Depends;
+
+		public DependsOnAttribute(params Type[] types)
+		{
+			Depends = types.ToList();
+		}
+	}
+}
---
+++ src/infrastructure/Damocles.Infrastructure/Damocles/Modules/IDmeModule.cs
@@ -0,0 +1,46 @@
+﻿// #region File Annotation
+// 
+// Author：Zhiqiang Li
+// 
+// FileName：IDmeModule.cs
+// 
+// Project：Damocles
+// 
+// CreateDate：2018年04月13日
+// 
+// ProjectUrl：https://github.com/stulzq/Damocles
+// 
+// .NET Core China Open Source organization  PMC member. https://github.com/dotnetcore
+// 
+// Note: The reference to this document code must not delete this note, and indicate the source!
+// 
+// #endregion
+
+namespace Damocles.Modules
+{
+	/// <summary>
+	/// 定义模块生命周期钩子
+	/// </summary>
+	public interface IDmeModule
+	{
+		/// <summary>
+		/// 预初始化
+		/// </summary>
+		void PreInitialize();
+
+		/// <summary>
+		/// 初始化
+		/// </summary>
+		void Initialize();
+
+		/// <summary>
+		/// 初始化完成
+		/// </summary>
+		void PostInitialize();
+
+		/// <summary>
+		/// 应用关闭时调用
+		/// </summary>
+		void Shutdown();
+	}
+}
